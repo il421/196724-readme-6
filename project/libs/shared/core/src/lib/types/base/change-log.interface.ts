fdd0723633812336cab6947a7d6077ed56507d7e
Ilya Suglobov <ilya.suglobov@bpsoftware.net>
2024-04-01 23:09:06 +1300

    added main types

---
+++ project/libs/shared/core/src/lib/types/base/change-log.interface.ts
@@ -0,0 +1,6 @@
+export interface ChangeLog {
+  createdBy: string;
+  createdAt: Date;
+  publishedBy?: string;
+  publishedAt?: Date;
+}
---
+++ project/libs/shared/core/src/lib/types/post/video-post.interface.ts
@@ -0,0 +1,5 @@
+import { Post } from './post.interface';
+
+export interface VideoPost extends Post {
+  url: string;
+}
