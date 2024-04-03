45e25057b5a2b7bd29f8867ae1fcc45a3f5034ab
Ilya Suglobov <ilya.suglobov@bpsoftware.net>
2024-04-03 23:25:55 +1300

    added more method for posts lib

--- project/libs/shared/core/src/types/subscription/subscription.type.ts
+++ project/libs/shared/core/src/types/subscription/subscription.type.ts
@@ -3,4 +3,5 @@ import { ChangeLog } from '../base';
 export type Subscription = Pick<ChangeLog, 'createdAt' | 'createdBy'> & {
   id?: string;
   publisherId: string;
+  lastSent?: string; // @TODO not sure about the extra field but looks like we need one
 };
