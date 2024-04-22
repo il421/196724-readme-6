37a0fef9e6fc2c3c72f9de929cd12adaceb8fccf
Ilya Suglobov <ilya.suglobov@bpsoftware.net>
2024-04-23 01:14:49 +1200

    added string parser

--- project/apps/posts/src/app/pipes/post-search-query-transform.pipe.ts
+++ project/apps/posts/src/app/pipes/post-search-query-transform.pipe.ts
@@ -21,7 +21,7 @@ export class PostSearchQueryTransformPipe implements PipeTransform {
     return {
       ...dto,
       tags: await arrayPipe.transform(dto?.tags ?? [], metadata),
-      usersIds: mongoIdValidationPipe.transform(dto?.usersIds ?? [], metadata),
+      usersIds: mongoIdValidationPipe.transform(dto?.usersIds, metadata),
       types: await arrayPipe.transform(dto?.types ?? [], metadata),
       limit: stringPipe.transform(dto.limit),
       page: stringPipe.transform(dto.page),
--- project/libs/shared/data-access/src/pipes/mongo-id-validation.pipe.ts
+++ project/libs/shared/data-access/src/pipes/mongo-id-validation.pipe.ts
@@ -9,13 +9,15 @@ import { ERROR_MESSAGES } from '@project/core';
 
 @Injectable()
 export class MongoIdValidationPipe implements PipeTransform {
-  public transform<T extends unknown = string | string[]>(
+  public transform<T extends unknown = string | string[] | undefined>(
     value: T,
     { type }: ArgumentMetadata
   ) {
     if (type === 'body') {
       throw new BadRequestException(ERROR_MESSAGES.PIPE);
     }
+
+    if (typeof value === 'undefined') return undefined;
 
     const isValueValid: boolean = Array.isArray(value)
       ? value.every((v) => Types.ObjectId.isValid(v))
