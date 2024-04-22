3df1ef9dba496896ef09b188eceadd11669536c0
Ilya Suglobov <ilya.suglobov@bpsoftware.net>
2024-04-23 01:17:23 +1200

    added string parser

--- project/apps/posts/src/app/post.constants.ts
+++ project/apps/posts/src/app/post.constants.ts
@@ -1,7 +1,7 @@
 import { ParseArrayOptions } from '@nestjs/common';
 
 export const DEFAULT_NUMBER_OF_POSTS = 25;
-export const DEFAULT_NUMBER_OF_POSTS_BY_TITLE = 20;
+
 export const PARSE_QUERY_ARRAY_PIPE_OPTIONS: ParseArrayOptions = {
   items: String,
   separator: ',',
